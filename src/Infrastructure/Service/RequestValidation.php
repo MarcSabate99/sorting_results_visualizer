76e81489fe8fbed4c09cae23e4128768d0426f8a
Marc Sabaté <masafecontact@gmail.com>
2024-02-01 20:16:46 +0100

    #6 Add to git history tracking

--- src/Infrastructure/Service/RequestValidation.php
+++ src/Infrastructure/Service/RequestValidation.php
@@ -12,20 +12,30 @@ class RequestValidation
     public function validateInternalFields(Request $request, array $requiredFields, array $internalFields): void
     {
         $content = json_decode($request->getContent(), true) ?? [];
+
         foreach ($requiredFields as $field) {
-            if (!isset($content[$field])) {
-                throw RequestBodyValidationException::ofRequiredParam($field);
-            }
+            $this->validateRequiredField($content, $field);
+
             foreach ($internalFields as $internalField) {
-                foreach ($content[$field] as $internalData) {
-                    if(!isset($internalData[$internalField])) {
-                        throw RequestBodyValidationException::ofRequiredParam($internalField);
-                    }
-                    if(empty($internalData[$internalField])){
-                        throw RequestBodyValidationException::ofRequiredParam($internalField);
-                    }
-                }
+                $this->validateInternalField($content[$field], $internalField);
             }
         }
     }
+
+    private function validateRequiredField(array $content, string $field): void
+    {
+        if (!isset($content[$field])) {
+            throw RequestBodyValidationException::ofRequiredParam($field);
+        }
+    }
+
+    private function validateInternalField(array $internalData, string $internalField): void
+    {
+        foreach ($internalData as $data) {
+            if (empty($data[$internalField])) {
+                throw RequestBodyValidationException::ofRequiredParam($internalField);
+            }
+        }
+    }
+
 }
